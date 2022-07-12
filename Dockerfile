3670a7c2972c32569625598f011f6095bbd25363
Hsun1031 <58354107+Hsun1031@users.noreply.github.com>
2022-07-12 10:15:19 +0000

    add google login

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM python:latest
 
+ENV PYTHONUNBUFFERED=1
+
 WORKDIR /python
 
-ADD python/ /python/
+ADD python/requirements.txt /python/requirements.txt
 
 RUN /bin/bash -c "/usr/local/bin/python -m pip install --upgrade pip && pip install -r /python/requirements.txt"
-
-CMD python /python/main.py
--- python/esp32/view.py
+++ python/esp32/view.py
@@ -3,6 +3,6 @@ from flask import Blueprint
 esp_blueprints = Blueprint('esp32', __name__)
 # esp_blueprints = Blueprint('esp32', __name__, template_folder= 'templates/esp32')
 
-@esp_blueprints.route('/setdata', methods=['GET','POST'])
+@esp_blueprints.route('/setdata', methods=['GET', 'POST'])
 def esp_set_data():
     return "OK"
