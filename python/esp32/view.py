fbb152608e630e27452ab2dbcfb35ad07d399a56
Hsun1031 <58354107+Hsun1031@users.noreply.github.com>
2022-07-16 03:19:59 +0000

    socket io

--- python/esp32/view.py
+++ python/esp32/view.py
@@ -3,13 +3,18 @@ from flask import Flask, Blueprint, render_template, request, redirect, url_for,
 
 esp_blueprints = Blueprint('esp32', __name__)
 # esp_blueprints = Blueprint('esp32', __name__, template_folder= 'templates/esp32')
+dnt11_now_data = {
+    'temp': None,
+    'hum': None
+    }
 
 @esp_blueprints.route('/setdata', methods=['GET', 'POST'])
 def esp_set_data():
     print("Content-Type:", request.headers.get('Content-Type'))
-    print("Json:", request.get_json())
     data = request.get_json()
-    print(data)
+    print("ESP32 setdata:", data)
+    dnt11_now_data['temp'] = data['temp']
+    dnt11_now_data['hum'] = data['hum']
     return "OK"
 
 @esp_blueprints.route('/form', methods=['GET', 'POST'])
